42bbf8a5e242868d538752dddf30ebe740af1789
呉屋 翔大 <goyasho0831@gmail.com>
2020-01-19 00:10:41 +0900

    docker run で flask 起動するようにした

--- backencd/app/run.py
+++ backencd/app/run.py
@@ -3,11 +3,12 @@ from flask import Flask, jsonify, request
 app = Flask(__name__)
 
 
-@app.route("/", methods=['GET'])
-def hello():
-    return "Hello World!"
+@app.route('/', methods=['GET'])
+@app.route("/<path>", methods=['GET'])
+def hello(path: str = '') -> str:
+    return "Hello World! {}".format(path)
 
 
 if __name__ == "__main__":
-    app.run()
-    print("run !!!")
+    print("## main ##")
+    app.run(host='0.0.0.0', port=5000, debug=True)
