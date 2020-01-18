d4b000b09f203795c56e55cc151161faf7b0de6b
呉屋 翔大 <goyasho0831@gmail.com>
2020-01-18 21:53:06 +0900

    flask で helloworld するだけの環境を構築した

---
+++ backencd/app/run.py
@@ -0,0 +1,13 @@
+from flask import Flask, jsonify, request
+
+app = Flask(__name__)
+
+
+@app.route("/", methods=['GET'])
+def hello():
+    return "Hello World!"
+
+
+if __name__ == "__main__":
+    app.run()
+    print("run !!!")
