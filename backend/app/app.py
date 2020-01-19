d8901d82e7684df0e1f69cd8ed8e3b5e38f1d293
呉屋 翔大 <goyasho0831@gmail.com>
2020-01-19 15:21:29 +0900

    yarn build してできた dist を backend から見れるようにした

--- backend/app/app.py
+++ backend/app/app.py
@@ -1,16 +1,24 @@
 import sys
-from flask import Flask
+from flask import Flask, render_template
 from lib import create_message
 
-app = Flask(__name__)
+app = Flask(__name__,
+            static_url_path='',
+            static_folder='../../dist',
+            template_folder='../../dist')
 
 sys.dont_write_bytecode = True
 
 
-@app.route('/', methods=['GET'])
-@app.route("/<path>", methods=['GET'])
-def hello(path: str = '') -> str:
-    print('#hello')
+@app.route('/')
+def web():
+    print('#web')
+    return render_template('index.html')
+
+
+@app.route("/api/<path>", methods=['GET'])
+def api(path: str = '') -> str:
+    print('#api')
     return create_message(path)
 
 
